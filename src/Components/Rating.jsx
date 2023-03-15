196c977ad877673368d567878c2b306a41a3161d
avizyt <avijit.biswas27@outlook.com>
2023-03-15 15:59:06 +0530

    rating updates

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Rating.module.css";
 // import { GiRoundStar } from "react-icons/gi";
 import { FaStar } from "react-icons/fa";
 
 function Rating() {
+    const [selectRating, setSelectRating] = useState();
+
+    function handleRatingClick(rating) {
+        setSelectRating(rating);
+    }
+
     return (
         <div className={styles.panel}>
             <FaStar className={styles.star} />
@@ -15,11 +21,25 @@ function Rating() {
             </p>
 
             <div className={styles.group}>
-                <button className={styles.rating}>1</button>
-                <button className={styles.rating}>2</button>
-                <button className={styles.rating}>3</button>
-                <button className={styles.rating}>4</button>
-                <button className={styles.rating}>5</button>
+                {[1, 2, 3, 4, 5].map((rating) => (
+                    <button
+                        onClick={() => handleRatingClick(1)}
+                        className={styles.rating}>
+                        {rating}
+                    </button>
+                ))}
+                {/* <button onClick={handleRatingClick} className={styles.rating}>
+                    2
+                </button>
+                <button onClick={handleRatingClick} className={styles.rating}>
+                    3
+                </button>
+                <button onClick={handleRatingClick} className={styles.rating}>
+                    4
+                </button>
+                <button onClick={handleRatingClick} className={styles.rating}>
+                    5
+                </button> */}
             </div>
 
             <button className={styles.submit}>Submit</button>
